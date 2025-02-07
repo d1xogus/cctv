e29d34882074b7fa8af3096a763266c05c2ff7d7
d1xogus <dlxo1012@naver.com>
2025-02-07 14:53:55 +0900

    요청인증허용

--- cctv/src/main/java/cctv/Config/SecurityConfig.java
+++ cctv/src/main/java/cctv/Config/SecurityConfig.java
@@ -27,8 +27,15 @@ public class SecurityConfig {
                 .formLogin(form -> form.disable()) // 폼 로그인 사용 Xs
 
                 .authorizeHttpRequests(auth -> auth // 요청에 인증 절차 필요
-                        .requestMatchers("/","/main", "/login").permitAll()// 루트 경로는 인증 절차 생략
+                        .requestMatchers("/","/main", "/oauth/**", "/login").permitAll()// 루트 경로는 인증 절차 생략
                         .anyRequest().authenticated() // 다른 모든 요청에 인증 필요a
+                )
+                .oauth2Login(oauth2 -> oauth2 // OAuth2를 통한 로그인 사용
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2Service) // 로그인 성공 시 사용자 서비스 로직 설정
+                        )
+                        .defaultSuccessUrl("http://localhost:3000/main", true) // 로그인 성공 시 이동할 URL 설정
+
                 )
                 .logout(logout -> logout // 로그아웃 설정
                         .logoutUrl("/logout") // 로그아웃 요청 URL (기본값: /logout)
@@ -37,13 +44,6 @@ public class SecurityConfig {
                         .deleteCookies("JSESSIONID") // JSESSIONID 쿠키 삭제
                         .clearAuthentication(true) // 인증 정보 삭제
                         .permitAll() // 로그아웃 요청은 인증 없이 접근 가능
-                )
-                .oauth2Login(oauth2 -> oauth2 // OAuth2를 통한 로그인 사용
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(oAuth2Service) // 로그인 성공 시 사용자 서비스 로직 설정
-                        )
-                        .defaultSuccessUrl("http://localhost:3000/main", true) // 로그인 성공 시 이동할 URL 설정
-
                 )
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
