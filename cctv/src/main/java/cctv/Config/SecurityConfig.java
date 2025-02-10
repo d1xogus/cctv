20eb68de641498c74e56b49fdc240a5ed73e0638
d1xogus <dlxo1012@naver.com>
2025-02-11 02:16:58 +0900

    authenticated test

--- cctv/src/main/java/cctv/Config/SecurityConfig.java
+++ cctv/src/main/java/cctv/Config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 안 함
                 .authorizeHttpRequests(auth -> auth // 요청에 인증 절차 필요
                         .requestMatchers("/","/main", "/oauth/**", "/login").permitAll()// 루트 경로는 인증 절차 생략
-                        .requestMatchers("/cctv/**").hasAuthority("ROLE_USER")
+                        .requestMatchers("/cctv/**").authenticated()
                         .anyRequest().authenticated() // 다른 모든 요청에 인증 필요a
                 )
                 .oauth2Login(oauth2 -> oauth2 // OAuth2를 통한 로그인 사용
