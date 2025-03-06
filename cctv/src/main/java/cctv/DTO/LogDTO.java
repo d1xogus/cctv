273a35774483299c2d5937567ab3c973b06c52b1
d1xogus <dlxo1012@naver.com>
2025-03-06 11:13:55 +0900

    logDTO 수정

--- cctv/src/main/java/cctv/DTO/LogDTO.java
+++ cctv/src/main/java/cctv/DTO/LogDTO.java
@@ -1,7 +1,11 @@
 package cctv.DTO;
 
+import cctv.Entity.Cctv;
 import cctv.Entity.Image;
 import cctv.Entity.Log;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,13 +13,21 @@ import lombok.Data;
 @Builder
 public class LogDTO {
     private Long logId; // Log ID
-    private Image image; // 연결된 Image
+    private Long imageId;
+    private String name;
+    private Cctv cctv;
+    private String path;
+    private String time;
     private String result; // 결과 메시지
 
     public static LogDTO toDTO(Log log) {
         return LogDTO.builder()
                 .logId(log.getLogId())
-                .image(log.getImage() != null ? log.getImage() : null)
+                .imageId(log.getImageId())
+                .name(log.getName())
+                .cctv(log.getCctv())
+                .path(log.getPath())
+                .time(log.getTime())
                 .result(log.getResult())
                 .build();
     }
